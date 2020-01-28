72490c357c973d8504b0b945c6f5045a1a489614
kjcdev <kjciastek@gmail.com>
2020-01-28 13:44:18 +0100

    Added new activities, made primary layout in MainActivity, established connection between class and buttons

--- app/src/main/java/com/example/englishteacher/MainActivity.java
+++ app/src/main/java/com/example/englishteacher/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.englishteacher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addBtn, learnBtn, aboutBtn, quitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void setButtons() {
+        addBtn = (Button) findViewById(R.id.addBtn);
+        learnBtn = (Button) findViewById(R.id.addBtn);
+        aboutBtn = (Button) findViewById(R.id.addBtn);
+        quitBtn = (Button) findViewById(R.id.addBtn);
     }
 }
