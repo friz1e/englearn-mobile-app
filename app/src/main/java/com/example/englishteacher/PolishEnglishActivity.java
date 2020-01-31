15b49921d54645a2724787e2c6f839e3751f1d6e
kjcdev <kjciastek@gmail.com>
2020-01-31 18:04:51 +0100

    Edited PolishEnglishActivity class

--- app/src/main/java/com/example/englishteacher/PolishEnglishActivity.java
+++ app/src/main/java/com/example/englishteacher/PolishEnglishActivity.java
@@ -2,13 +2,112 @@ package com.example.englishteacher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class PolishEnglishActivity extends AppCompatActivity {
+
+    TextView questionWordTV, answerWordTV;
+    EditText enterET;
+    Button startBtn, checkBtn, nextBtn;
+
+    private void setComponents() {
+        startBtn = (Button) findViewById(R.id.startBtn);
+        questionWordTV = (TextView) findViewById(R.id.questionWordTV);
+        answerWordTV = (TextView) findViewById(R.id.answerWordTV);
+        enterET = (EditText) findViewById(R.id.enterET);
+        checkBtn = (Button) findViewById(R.id.checkBtn);
+        nextBtn = (Button) findViewById(R.id.nextBtn);
+    }
+
+    DatabaseHelper wordsDatabase;
+    String foreignLanguageWord;
+    String nativeLanguageWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_polish_english);
+
+        setComponents();
+
+        questionWordTV.setVisibility(View.INVISIBLE);
+        answerWordTV.setVisibility(View.INVISIBLE);
+        enterET.setVisibility(View.INVISIBLE);
+        checkBtn.setVisibility(View.INVISIBLE);
+        nextBtn.setVisibility(View.INVISIBLE);
+
+        wordsDatabase = new DatabaseHelper(this);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor response = wordsDatabase.getRandomWordsPair();
+                if(response.moveToFirst()) {
+                    do {
+                        foreignLanguageWord = response.getString(0);
+                        nativeLanguageWord = response.getString(1);
+                    } while (response.moveToNext());
+                }
+                response.close();
+                wordsDatabase.close();
+
+                questionWordTV.setText(nativeLanguageWord);
+                if(!questionWordTV.getText().toString().equals("")) {
+                    startBtn.setVisibility(View.INVISIBLE);
+                    questionWordTV.setVisibility(View.VISIBLE);
+                    enterET.setVisibility(View.VISIBLE);
+                    checkBtn.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        final ColorStateList oldColors = questionWordTV.getTextColors();
+
+        checkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (foreignLanguageWord.equals(enterET.getText().toString())) {
+                    questionWordTV.setTextColor(Color.GREEN);
+                    checkBtn.setVisibility(View.INVISIBLE);
+                    nextBtn.setVisibility(View.VISIBLE);
+                } else {
+                    questionWordTV.setTextColor(Color.RED);
+                    answerWordTV.setText(foreignLanguageWord);
+                    answerWordTV.setVisibility(View.VISIBLE);
+                    checkBtn.setVisibility(View.INVISIBLE);
+                    nextBtn.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor response = wordsDatabase.getRandomWordsPair();
+                if(response.moveToFirst()) {
+                    do {
+                        foreignLanguageWord = response.getString(0);
+                        nativeLanguageWord = response.getString(1);
+                    } while (response.moveToNext());
+                }
+                response.close();
+                wordsDatabase.close();
+
+                questionWordTV.setTextColor(oldColors);
+                questionWordTV.setText(nativeLanguageWord);
+                answerWordTV.setText("");
+
+                nextBtn.setVisibility(View.INVISIBLE);
+                checkBtn.setVisibility(View.VISIBLE);
+            }
+        });
     }
+
 }
